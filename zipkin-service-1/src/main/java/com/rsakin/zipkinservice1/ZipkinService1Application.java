536d395557efd85b5393ae25fee89a44d72d75f7
r84160880 <ramazan.sakin@huawei.com>
2020-09-06 16:20:08 +0300

    Replacements

--- zipkin-service-1/src/main/java/com/rsakin/zipkinservice1/ZipkinService1Application.java
+++ zipkin-service-1/src/main/java/com/rsakin/zipkinservice1/ZipkinService1Application.java
@@ -1,4 +1,4 @@
-package com.example.zipkinservice1;
+package com.rsakin.zipkinservice1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- zipkin-service-1/src/main/java/com/rsakin/zipkinservice1/controller/ZipkinController.java
+++ zipkin-service-1/src/main/java/com/rsakin/zipkinservice1/controller/ZipkinController.java
@@ -1,4 +1,4 @@
-package com.example.zipkinservice1.controller;
+package com.rsakin.zipkinservice1.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
